85e7caad96a5702acf0f2c115c6b7b5d50fea432
huangxiaoqing1028 <1367698055@qq.com>
2026-05-09 17:22:55 +0800

    Improve UI with adaptive Auto Layout and add explicit back navigation across pages

--- HeavySweep/HeavySweep/Sources/GameViewController.m
+++ HeavySweep/HeavySweep/Sources/GameViewController.m
@@ -4,5 +4,14 @@
 #import "ProgressManager.h"
 @implementation GameViewController
 - (void)loadView { self.view = [[SKView alloc] initWithFrame:UIScreen.mainScreen.bounds]; }
-- (void)viewDidLoad { [super viewDidLoad]; SKView *v=(SKView*)self.view; GameScene*s=[[GameScene alloc] initWithSize:v.bounds.size]; s.gameFinished=^(NSInteger score, NSInteger goldGain){ ProgressManager*p=[ProgressManager shared]; p.gold+=goldGain; p.bestScore=MAX(p.bestScore,score); if(score>=1000)[p.achievements addObject:@"war_god"]; [p save]; [self.navigationController popViewControllerAnimated:YES]; }; [v presentScene:s]; }
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIButton *back=[UIButton buttonWithType:UIButtonTypeSystem]; back.translatesAutoresizingMaskIntoConstraints=NO; [back setTitle:@"← 返回" forState:UIControlStateNormal]; [back setTitleColor:UIColor.whiteColor forState:UIControlStateNormal]; [back addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside]; [self.view addSubview:back];
+    [NSLayoutConstraint activateConstraints:@[[back.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:12],[back.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:6]]];
+
+    SKView *v=(SKView*)self.view; GameScene*s=[[GameScene alloc] initWithSize:v.bounds.size];
+    s.gameFinished=^(NSInteger score, NSInteger goldGain){ ProgressManager*p=[ProgressManager shared]; p.gold+=goldGain; p.bestScore=MAX(p.bestScore,score); if(score>=1000)[p.achievements addObject:@"war_god"]; [p save]; [self.navigationController popViewControllerAnimated:YES]; };
+    [v presentScene:s];
+}
+- (void)goBack { [self.navigationController popViewControllerAnimated:YES]; }
 @end
--- HeavySweep/HeavySweep/Sources/SettingsViewController.m
+++ HeavySweep/HeavySweep/Sources/SettingsViewController.m
@@ -10,25 +10,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor=UIColor.systemBackgroundColor;
-    self.title=@"设置";
+
+    UIButton *back=[UIButton buttonWithType:UIButtonTypeSystem];
+    back.translatesAutoresizingMaskIntoConstraints=NO;
+    [back setTitle:@"← 返回" forState:UIControlStateNormal];
+    [back addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:back];
+
+    UILabel *title=[UILabel new]; title.translatesAutoresizingMaskIntoConstraints=NO; title.text=@"设置"; title.font=[UIFont boldSystemFontOfSize:24]; [self.view addSubview:title];
+
     GameConfig *c=[GameConfig shared];
+    self.sfxSlider=[UISlider new]; self.sfxSlider.translatesAutoresizingMaskIntoConstraints=NO; self.sfxSlider.value=c.sfxVolume;
+    [self.sfxSlider addTarget:self action:@selector(onSfxChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.sfxSlider];
 
-    self.sfxSlider=[[UISlider alloc]initWithFrame:CGRectMake(30,140,self.view.bounds.size.width-60,30)];
-    self.sfxSlider.value=c.sfxVolume;
-    [self.sfxSlider addTarget:self action:@selector(onSfxChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:self.sfxSlider];
+    self.hapticSwitch=[UISwitch new]; self.hapticSwitch.translatesAutoresizingMaskIntoConstraints=NO; self.hapticSwitch.on=c.hapticsEnabled;
+    [self.hapticSwitch addTarget:self action:@selector(onHapticChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.hapticSwitch];
 
-    self.hapticSwitch=[[UISwitch alloc]initWithFrame:CGRectMake(30,200,80,40)];
-    self.hapticSwitch.on=c.hapticsEnabled;
-    [self.hapticSwitch addTarget:self action:@selector(onHapticChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:self.hapticSwitch];
+    UITextView *privacy=[UITextView new]; privacy.translatesAutoresizingMaskIntoConstraints=NO; privacy.editable=NO; privacy.font=[UIFont systemFontOfSize:16];
+    privacy.text=@"隐私说明：本游戏为单机，不收集个人数据，不接入广告追踪，不请求通讯录/定位权限。"; [self.view addSubview:privacy];
 
-    UITextView *privacy=[[UITextView alloc]initWithFrame:CGRectMake(20,270,self.view.bounds.size.width-40,300)];
-    privacy.editable=NO;
-    privacy.text=@"隐私说明：本游戏为单机，不收集个人数据，不接入广告追踪，不请求通讯录/定位权限。";
-    [self.view addSubview:privacy];
+    UILayoutGuide *g=self.view.safeAreaLayoutGuide;
+    [NSLayoutConstraint activateConstraints:@[
+        [back.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:16], [back.topAnchor constraintEqualToAnchor:g.topAnchor constant:8],
+        [title.centerXAnchor constraintEqualToAnchor:g.centerXAnchor], [title.centerYAnchor constraintEqualToAnchor:back.centerYAnchor],
+        [self.sfxSlider.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:24], [self.sfxSlider.trailingAnchor constraintEqualToAnchor:g.trailingAnchor constant:-24], [self.sfxSlider.topAnchor constraintEqualToAnchor:title.bottomAnchor constant:36],
+        [self.hapticSwitch.leadingAnchor constraintEqualToAnchor:self.sfxSlider.leadingAnchor], [self.hapticSwitch.topAnchor constraintEqualToAnchor:self.sfxSlider.bottomAnchor constant:26],
+        [privacy.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:20], [privacy.trailingAnchor constraintEqualToAnchor:g.trailingAnchor constant:-20], [privacy.topAnchor constraintEqualToAnchor:self.hapticSwitch.bottomAnchor constant:24], [privacy.bottomAnchor constraintLessThanOrEqualToAnchor:g.bottomAnchor constant:-20]
+    ]];
 }
-
+- (void)goBack { [self.navigationController popViewControllerAnimated:YES]; }
 - (void)onSfxChanged:(UISlider *)sender { GameConfig *c=[GameConfig shared]; c.sfxVolume=sender.value; [c save]; }
 - (void)onHapticChanged:(UISwitch *)sender { GameConfig *c=[GameConfig shared]; c.hapticsEnabled=sender.isOn; [c save]; }
 @end
