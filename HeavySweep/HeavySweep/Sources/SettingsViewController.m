69dbdf74fc01ba7345a0df26204a678390960602
huangxiaoqing1028 <1367698055@qq.com>
2026-05-09 16:56:19 +0800

    Fix runtime/compile issues by replacing UIAction sender usage with target-action

--- HeavySweep/HeavySweep/Sources/SettingsViewController.m
+++ HeavySweep/HeavySweep/Sources/SettingsViewController.m
@@ -1,5 +1,34 @@
 #import "SettingsViewController.h"
 #import "GameConfig.h"
+
+@interface SettingsViewController ()
+@property (nonatomic, strong) UISlider *sfxSlider;
+@property (nonatomic, strong) UISwitch *hapticSwitch;
+@end
+
 @implementation SettingsViewController
-- (void)viewDidLoad { [super viewDidLoad]; self.view.backgroundColor=UIColor.systemBackgroundColor; self.title=@"设置"; GameConfig*c=[GameConfig shared]; UISlider*sfx=[[UISlider alloc]initWithFrame:CGRectMake(30,140,self.view.bounds.size.width-60,30)]; sfx.value=c.sfxVolume; [sfx addAction:[UIAction actionWithHandler:^(__kindof UIAction * _Nonnull action) { c.sfxVolume=((UISlider*)action.sender).value; [c save]; }] forControlEvents:UIControlEventValueChanged]; [self.view addSubview:sfx]; UISwitch*h=[[UISwitch alloc]initWithFrame:CGRectMake(30,200,80,40)]; h.on=c.hapticsEnabled; [h addAction:[UIAction actionWithHandler:^(__kindof UIAction * _Nonnull action) { c.hapticsEnabled=((UISwitch*)action.sender).isOn; [c save]; }] forControlEvents:UIControlEventValueChanged]; [self.view addSubview:h]; UITextView*privacy=[[UITextView alloc]initWithFrame:CGRectMake(20,270,self.view.bounds.size.width-40,300)]; privacy.editable=NO; privacy.text=@"隐私说明：本游戏为单机，不收集个人数据，不接入广告追踪，不请求通讯录/定位权限。"; [self.view addSubview:privacy]; }
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=UIColor.systemBackgroundColor;
+    self.title=@"设置";
+    GameConfig *c=[GameConfig shared];
+
+    self.sfxSlider=[[UISlider alloc]initWithFrame:CGRectMake(30,140,self.view.bounds.size.width-60,30)];
+    self.sfxSlider.value=c.sfxVolume;
+    [self.sfxSlider addTarget:self action:@selector(onSfxChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.sfxSlider];
+
+    self.hapticSwitch=[[UISwitch alloc]initWithFrame:CGRectMake(30,200,80,40)];
+    self.hapticSwitch.on=c.hapticsEnabled;
+    [self.hapticSwitch addTarget:self action:@selector(onHapticChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.hapticSwitch];
+
+    UITextView *privacy=[[UITextView alloc]initWithFrame:CGRectMake(20,270,self.view.bounds.size.width-40,300)];
+    privacy.editable=NO;
+    privacy.text=@"隐私说明：本游戏为单机，不收集个人数据，不接入广告追踪，不请求通讯录/定位权限。";
+    [self.view addSubview:privacy];
+}
+
+- (void)onSfxChanged:(UISlider *)sender { GameConfig *c=[GameConfig shared]; c.sfxVolume=sender.value; [c save]; }
+- (void)onHapticChanged:(UISwitch *)sender { GameConfig *c=[GameConfig shared]; c.hapticsEnabled=sender.isOn; [c save]; }
 @end
