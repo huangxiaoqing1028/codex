b4c31cc21895fe272bd8f89dd6da47372d84f341
huangxiaoqing1028 <1367698055@qq.com>
2026-05-09 17:33:24 +0800

    Fix notch-safe HUD and countdown update logic; expand settings features

--- HeavySweep/HeavySweep/Sources/GameScene.m
+++ HeavySweep/HeavySweep/Sources/GameScene.m
@@ -1,11 +1,76 @@
 #import "GameScene.h"
+
 @interface GameScene ()
-@property NSInteger score; @property NSTimeInterval timeLeft; @property SKLabelNode *hud;
+@property NSInteger score;
+@property NSTimeInterval timeLeft;
+@property NSTimeInterval lastUpdateTime;
+@property SKLabelNode *scoreLabel;
+@property SKLabelNode *timerLabel;
 @end
+
 @implementation GameScene
-- (void)didMoveToView:(SKView *)view { self.backgroundColor=[SKColor blackColor]; self.timeLeft=90; self.hud=[SKLabelNode labelNodeWithFontNamed:@"AvenirNext-Bold"]; self.hud.position=CGPointMake(self.size.width/2,self.size.height-60); [self addChild:self.hud]; [self spawn]; [self refresh]; }
-- (void)spawn { if(self.timeLeft<=0)return; SKShapeNode*t=[SKShapeNode shapeNodeWithCircleOfRadius:18+arc4random_uniform(22)]; t.fillColor=[SKColor colorWithRed:0.8 green:0.2+drand48()*0.4 blue:0.2 alpha:1]; t.position=CGPointMake(40+arc4random_uniform(self.size.width-80),160+arc4random_uniform(self.size.height-240)); t.name=@"mob"; [self addChild:t]; [t runAction:[SKAction sequence:@[[SKAction waitForDuration:1.6],[SKAction removeFromParent]]]]; [self runAction:[SKAction sequence:@[[SKAction waitForDuration:0.28],[SKAction runBlock:^{[self spawn];}]]]]; }
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event { CGPoint p=[[touches anyObject] locationInNode:self]; SKNode*n=[self nodeAtPoint:p]; if([n.name isEqualToString:@"mob"]){self.score+=15; [n removeFromParent];} else self.score=MAX(0,self.score-4); [self refresh]; }
-- (void)update:(NSTimeInterval)currentTime { static NSTimeInterval last=0; if(!last){last=currentTime;return;} self.timeLeft-=currentTime-last; last=currentTime; if(self.timeLeft<=0){ [self removeAllActions]; NSInteger gold=80+self.score/3; if(self.gameFinished) self.gameFinished(self.score,gold);} [self refresh]; }
-- (void)refresh { self.hud.text=[NSString stringWithFormat:@"战力:%ld  倒计时:%02.0f",(long)self.score,MAX(0,ceil(self.timeLeft))]; }
+- (void)didMoveToView:(SKView *)view {
+    self.backgroundColor=[SKColor blackColor];
+    self.timeLeft=90;
+    self.lastUpdateTime=0;
+
+    CGFloat topInset = 0;
+    if (@available(iOS 11.0, *)) { topInset = view.safeAreaInsets.top; }
+    CGFloat hudY = self.size.height - MAX(56, topInset + 28);
+
+    self.scoreLabel=[SKLabelNode labelNodeWithFontNamed:@"AvenirNext-Bold"];
+    self.scoreLabel.fontSize=22;
+    self.scoreLabel.horizontalAlignmentMode=SKLabelHorizontalAlignmentModeLeft;
+    self.scoreLabel.position=CGPointMake(20, hudY);
+    [self addChild:self.scoreLabel];
+
+    self.timerLabel=[SKLabelNode labelNodeWithFontNamed:@"AvenirNext-Bold"];
+    self.timerLabel.fontSize=22;
+    self.timerLabel.horizontalAlignmentMode=SKLabelHorizontalAlignmentModeRight;
+    self.timerLabel.position=CGPointMake(self.size.width-20, hudY);
+    [self addChild:self.timerLabel];
+
+    [self spawn];
+    [self refresh];
+}
+
+- (void)spawn {
+    if(self.timeLeft<=0)return;
+    SKShapeNode*t=[SKShapeNode shapeNodeWithCircleOfRadius:18+arc4random_uniform(22)];
+    t.fillColor=[SKColor colorWithRed:0.8 green:0.2+drand48()*0.4 blue:0.2 alpha:1];
+    t.position=CGPointMake(40+arc4random_uniform((uint32_t)MAX(1,self.size.width-80)),160+arc4random_uniform((uint32_t)MAX(1,self.size.height-240)));
+    t.name=@"mob";
+    [self addChild:t];
+    [t runAction:[SKAction sequence:@[[SKAction waitForDuration:1.6],[SKAction removeFromParent]]]];
+    [self runAction:[SKAction sequence:@[[SKAction waitForDuration:0.28],[SKAction runBlock:^{[self spawn];}]]]];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    CGPoint p=[[touches anyObject] locationInNode:self];
+    SKNode*n=[self nodeAtPoint:p];
+    if([n.name isEqualToString:@"mob"]){self.score+=15; [n removeFromParent];}
+    else self.score=MAX(0,self.score-4);
+    [self refresh];
+}
+
+- (void)update:(NSTimeInterval)currentTime {
+    if(self.lastUpdateTime==0){ self.lastUpdateTime=currentTime; return; }
+    NSTimeInterval dt = currentTime-self.lastUpdateTime;
+    self.lastUpdateTime=currentTime;
+
+    if(self.timeLeft>0){
+        self.timeLeft = MAX(0,self.timeLeft-dt);
+        if(self.timeLeft<=0){
+            [self removeAllActions];
+            NSInteger gold=80+self.score/3;
+            if(self.gameFinished) self.gameFinished(self.score,gold);
+        }
+        [self refresh];
+    }
+}
+
+- (void)refresh {
+    self.scoreLabel.text=[NSString stringWithFormat:@"战力:%ld",(long)self.score];
+    self.timerLabel.text=[NSString stringWithFormat:@"倒计时:%02.0f",ceil(self.timeLeft)];
+}
 @end
--- HeavySweep/HeavySweep/Sources/SettingsViewController.m
+++ HeavySweep/HeavySweep/Sources/SettingsViewController.m
@@ -1,8 +1,10 @@
 #import "SettingsViewController.h"
 #import "GameConfig.h"
+#import "ProgressManager.h"
 
 @interface SettingsViewController ()
 @property (nonatomic, strong) UISlider *sfxSlider;
+@property (nonatomic, strong) UISlider *musicSlider;
 @property (nonatomic, strong) UISwitch *hapticSwitch;
 @end
 
@@ -20,25 +22,33 @@
     UILabel *title=[UILabel new]; title.translatesAutoresizingMaskIntoConstraints=NO; title.text=@"设置"; title.font=[UIFont boldSystemFontOfSize:24]; [self.view addSubview:title];
 
     GameConfig *c=[GameConfig shared];
-    self.sfxSlider=[UISlider new]; self.sfxSlider.translatesAutoresizingMaskIntoConstraints=NO; self.sfxSlider.value=c.sfxVolume;
-    [self.sfxSlider addTarget:self action:@selector(onSfxChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.sfxSlider];
+    UILabel *sfx=[UILabel new]; sfx.translatesAutoresizingMaskIntoConstraints=NO; sfx.text=@"音效音量"; [self.view addSubview:sfx];
+    self.sfxSlider=[UISlider new]; self.sfxSlider.translatesAutoresizingMaskIntoConstraints=NO; self.sfxSlider.value=c.sfxVolume; [self.sfxSlider addTarget:self action:@selector(onSfxChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.sfxSlider];
 
-    self.hapticSwitch=[UISwitch new]; self.hapticSwitch.translatesAutoresizingMaskIntoConstraints=NO; self.hapticSwitch.on=c.hapticsEnabled;
-    [self.hapticSwitch addTarget:self action:@selector(onHapticChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.hapticSwitch];
+    UILabel *music=[UILabel new]; music.translatesAutoresizingMaskIntoConstraints=NO; music.text=@"音乐音量"; [self.view addSubview:music];
+    self.musicSlider=[UISlider new]; self.musicSlider.translatesAutoresizingMaskIntoConstraints=NO; self.musicSlider.value=c.musicVolume; [self.musicSlider addTarget:self action:@selector(onMusicChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.musicSlider];
 
-    UITextView *privacy=[UITextView new]; privacy.translatesAutoresizingMaskIntoConstraints=NO; privacy.editable=NO; privacy.font=[UIFont systemFontOfSize:16];
-    privacy.text=@"隐私说明：本游戏为单机，不收集个人数据，不接入广告追踪，不请求通讯录/定位权限。"; [self.view addSubview:privacy];
+    UILabel *h=[UILabel new]; h.translatesAutoresizingMaskIntoConstraints=NO; h.text=@"震动反馈"; [self.view addSubview:h];
+    self.hapticSwitch=[UISwitch new]; self.hapticSwitch.translatesAutoresizingMaskIntoConstraints=NO; self.hapticSwitch.on=c.hapticsEnabled; [self.hapticSwitch addTarget:self action:@selector(onHapticChanged:) forControlEvents:UIControlEventValueChanged]; [self.view addSubview:self.hapticSwitch];
+
+    UIButton *reset=[UIButton buttonWithType:UIButtonTypeSystem]; reset.translatesAutoresizingMaskIntoConstraints=NO; [reset setTitle:@"重置本地进度" forState:UIControlStateNormal]; [reset setTitleColor:UIColor.systemRedColor forState:UIControlStateNormal]; [reset addTarget:self action:@selector(resetProgress) forControlEvents:UIControlEventTouchUpInside]; [self.view addSubview:reset];
 
     UILayoutGuide *g=self.view.safeAreaLayoutGuide;
     [NSLayoutConstraint activateConstraints:@[
         [back.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:16], [back.topAnchor constraintEqualToAnchor:g.topAnchor constant:8],
         [title.centerXAnchor constraintEqualToAnchor:g.centerXAnchor], [title.centerYAnchor constraintEqualToAnchor:back.centerYAnchor],
-        [self.sfxSlider.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:24], [self.sfxSlider.trailingAnchor constraintEqualToAnchor:g.trailingAnchor constant:-24], [self.sfxSlider.topAnchor constraintEqualToAnchor:title.bottomAnchor constant:36],
-        [self.hapticSwitch.leadingAnchor constraintEqualToAnchor:self.sfxSlider.leadingAnchor], [self.hapticSwitch.topAnchor constraintEqualToAnchor:self.sfxSlider.bottomAnchor constant:26],
-        [privacy.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:20], [privacy.trailingAnchor constraintEqualToAnchor:g.trailingAnchor constant:-20], [privacy.topAnchor constraintEqualToAnchor:self.hapticSwitch.bottomAnchor constant:24], [privacy.bottomAnchor constraintLessThanOrEqualToAnchor:g.bottomAnchor constant:-20]
+        [sfx.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:24],[sfx.topAnchor constraintEqualToAnchor:title.bottomAnchor constant:28],
+        [self.sfxSlider.leadingAnchor constraintEqualToAnchor:g.leadingAnchor constant:24], [self.sfxSlider.trailingAnchor constraintEqualToAnchor:g.trailingAnchor constant:-24], [self.sfxSlider.topAnchor constraintEqualToAnchor:sfx.bottomAnchor constant:8],
+        [music.leadingAnchor constraintEqualToAnchor:self.sfxSlider.leadingAnchor],[music.topAnchor constraintEqualToAnchor:self.sfxSlider.bottomAnchor constant:18],
+        [self.musicSlider.leadingAnchor constraintEqualToAnchor:self.sfxSlider.leadingAnchor],[self.musicSlider.trailingAnchor constraintEqualToAnchor:self.sfxSlider.trailingAnchor],[self.musicSlider.topAnchor constraintEqualToAnchor:music.bottomAnchor constant:8],
+        [h.leadingAnchor constraintEqualToAnchor:self.sfxSlider.leadingAnchor],[h.topAnchor constraintEqualToAnchor:self.musicSlider.bottomAnchor constant:22],
+        [self.hapticSwitch.leadingAnchor constraintEqualToAnchor:h.trailingAnchor constant:16],[self.hapticSwitch.centerYAnchor constraintEqualToAnchor:h.centerYAnchor],
+        [reset.topAnchor constraintEqualToAnchor:h.bottomAnchor constant:30],[reset.leadingAnchor constraintEqualToAnchor:self.sfxSlider.leadingAnchor]
     ]];
 }
 - (void)goBack { [self.navigationController popViewControllerAnimated:YES]; }
 - (void)onSfxChanged:(UISlider *)sender { GameConfig *c=[GameConfig shared]; c.sfxVolume=sender.value; [c save]; }
+- (void)onMusicChanged:(UISlider *)sender { GameConfig *c=[GameConfig shared]; c.musicVolume=sender.value; [c save]; }
 - (void)onHapticChanged:(UISwitch *)sender { GameConfig *c=[GameConfig shared]; c.hapticsEnabled=sender.isOn; [c save]; }
+- (void)resetProgress { ProgressManager *p=[ProgressManager shared]; p.gold=0; p.bestScore=0; [p.achievements removeAllObjects]; [p save]; }
 @end
