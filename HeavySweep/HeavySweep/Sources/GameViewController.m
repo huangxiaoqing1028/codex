718f4bcffc5ecad4869fad139c03857fbfc95c36
huangxiaoqing1028 <1367698055@qq.com>
2026-05-09 17:41:14 +0800

    Refactor and format UI controller code for readability and consistency

--- HeavySweep/HeavySweep/Sources/GameViewController.m
+++ HeavySweep/HeavySweep/Sources/GameViewController.m
@@ -1,17 +1,48 @@
 #import "GameViewController.h"
+
 #import <SpriteKit/SpriteKit.h>
+
 #import "GameScene.h"
 #import "ProgressManager.h"
+
 @implementation GameViewController
-- (void)loadView { self.view = [[SKView alloc] initWithFrame:UIScreen.mainScreen.bounds]; }
+
+- (void)loadView {
+    self.view = [[SKView alloc] initWithFrame:UIScreen.mainScreen.bounds];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIButton *back=[UIButton buttonWithType:UIButtonTypeSystem]; back.translatesAutoresizingMaskIntoConstraints=NO; [back setTitle:@"← 返回" forState:UIControlStateNormal]; [back setTitleColor:UIColor.whiteColor forState:UIControlStateNormal]; [back addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside]; [self.view addSubview:back];
-    [NSLayoutConstraint activateConstraints:@[[back.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:12],[back.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:6]]];
 
-    SKView *v=(SKView*)self.view; GameScene*s=[[GameScene alloc] initWithSize:v.bounds.size];
-    s.gameFinished=^(NSInteger score, NSInteger goldGain){ ProgressManager*p=[ProgressManager shared]; p.gold+=goldGain; p.bestScore=MAX(p.bestScore,score); if(score>=1000)[p.achievements addObject:@"war_god"]; [p save]; [self.navigationController popViewControllerAnimated:YES]; };
-    [v presentScene:s];
+    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    backButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [backButton setTitle:@"← 返回" forState:UIControlStateNormal];
+    [backButton setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    [backButton addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:backButton];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [backButton.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:12],
+        [backButton.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:6]
+    ]];
+
+    SKView *skView = (SKView *)self.view;
+    GameScene *scene = [[GameScene alloc] initWithSize:skView.bounds.size];
+    scene.gameFinished = ^(NSInteger score, NSInteger goldGain) {
+        ProgressManager *progress = [ProgressManager shared];
+        progress.gold += goldGain;
+        progress.bestScore = MAX(progress.bestScore, score);
+        if (score >= 1000) {
+            [progress.achievements addObject:@"war_god"];
+        }
+        [progress save];
+        [self.navigationController popViewControllerAnimated:YES];
+    };
+    [skView presentScene:scene];
 }
-- (void)goBack { [self.navigationController popViewControllerAnimated:YES]; }
+
+- (void)goBack {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 @end
